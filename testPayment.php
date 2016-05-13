e7927d1fbfc91a6fcbe75750e9120bb7ec974d60
Li meng <li487@iupui.edu>
2016-05-13 10:19:58 +0800

    2016-5-13

---
+++ testPayment.php
@@ -0,0 +1,14 @@
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="DZ6KW9MJ6XAKQ">
+<table>
+<tr><td><input type="hidden" name="on0" value="three items">three items</td></tr><tr><td><select name="os0">
+	<option value="a">a $1.00 USD</option>
+	<option value="b">b $2.00 USD</option>
+	<option value="c">c $3.00 USD</option>
+</select> </td></tr>
+</table>
+<input type="hidden" name="currency_code" value="USD">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
